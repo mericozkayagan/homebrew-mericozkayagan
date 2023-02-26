60442938d414510a73d94180c392094325b1ddf0
mericozkayagan <mericozkayagan@gmail.com>
2023-02-26 16:44:39 +0300

    Brew formula update for minecraft version v0.1.3

--- minecraft.rb
+++ minecraft.rb
@@ -5,11 +5,11 @@
 class Minecraft < Formula
   desc ""
   homepage "https://github.com/mericozkayagan/minecraft"
-  version "0.1.2"
+  version "0.1.3"
 
   on_macos do
-    url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.2/minecraft_0.1.2_darwin_all.tar.gz"
-    sha256 "b884720b2113d0a73a46854318652ffb704bb54dd49bd3cc2afd3993ef558bfe"
+    url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.3/minecraft_0.1.3_darwin_all.tar.gz"
+    sha256 "227fec958543ce1f18b5fd7d5509d9da3fbf7268a8453ba3cca7aff768a3cc11"
 
     def install
       bin.install "minecraft"
@@ -18,16 +18,16 @@ class Minecraft < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.2/minecraft_0.1.2_linux_arm64.tar.gz"
-      sha256 "54eed57ec14e6152b32442cab68ba38a743683cee964ea69d70e34fd71737ace"
+      url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.3/minecraft_0.1.3_linux_arm64.tar.gz"
+      sha256 "0fbb850cb8dd0e006432857c5858114b85a7a54fab11595b79e16de734355996"
 
       def install
         bin.install "minecraft"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.2/minecraft_0.1.2_linux_amd64.tar.gz"
-      sha256 "7cb0943da248e1773e196dfc1aec2878c856af6b1d9a825eaf3c164bb7c915f5"
+      url "https://github.com/mericozkayagan/minecraft/releases/download/v0.1.3/minecraft_0.1.3_linux_amd64.tar.gz"
+      sha256 "9dabd78f74f0b717eb5c7c4b8ca852850eafbfe1a1cdaf62568c49b8195b5d00"
 
       def install
         bin.install "minecraft"
